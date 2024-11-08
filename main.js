0d7d269bb21e34749fff315dcb8d1cd24262889d
Naglaa99 <Naglaahamdi6@gmail.com>
2024-11-08 23:44:02 +0200

    create GameX project

---
+++ main.js
@@ -0,0 +1,223 @@
+const navbarLinks = document.querySelectorAll(".nav-link");
+const navSocialList = document.querySelectorAll(".nav-social-list li a");
+const openMenuIcon = document.querySelector(".open-menu i");
+const closeMenuIcon = document.querySelector(".close-menu i");
+const navbar = document.querySelector("nav");
+const overlay = document.querySelector(".overlay");
+const h1 = document.querySelectorAll("h1");
+const headers = document.querySelectorAll("h2,h3");
+const container = document.querySelectorAll(".container");
+const button = document.querySelectorAll(".button");
+const primaryButton = document.querySelectorAll(".primary-button");
+const galleryList = document.querySelectorAll(".gallery-list li");
+const teamMemberList = document.querySelectorAll(".team-member-list li");
+const teamMember = document.querySelectorAll(".team-member-list li a");
+const teamMemberImage = document.querySelectorAll(".team-member-list a img");
+const materialSymbolsOutlined = document.querySelectorAll(
+  ".material-symbols-outlined"
+);
+const cardButton = document.querySelectorAll(".card-button");
+const footerMenuLink = document.querySelectorAll(".footer-menu-list li");
+const searchButton = document.querySelector(".footer-search-button");
+const footerQuickLinksList = document.querySelectorAll(
+  ".footer-quicklinks-list li"
+);
+const footerSocialItem = document.querySelectorAll(".footer-social-list li");
+const goTopButton = document.querySelector(".go-top");
+const applyUiStyle = () => {
+  navSocialList.forEach((item) => {
+    item.classList.add("text-orange", "text-[20px]");
+  });
+  navbarLinks.forEach((item) => {
+    item.classList.add(
+      "text-white",
+      "py-[10px]",
+      "px-[25px]",
+      "text-fs-8",
+      "border-b-[1px]",
+      "font-medium",
+      "transition",
+      "duration-[.15s]",
+      "ease-in-out",
+      "border-custom",
+      "border-solid",
+      "capitalize",
+      "cursor-pointer",
+      "hover:text-orange"
+    );
+    item.addEventListener("click", () => {
+      navbar.classList.add("hidden");
+    });
+  });
+
+  h1.forEach((style) => {
+    style.classList.add(
+      "text-white",
+      "font-refault",
+      "text-fs-1",
+      "font-normal",
+      "uppercase"
+    );
+  });
+  headers.forEach((style) => {
+    style.classList.add(
+      "text-white",
+      "font-oswald",
+      "text-fs-2",
+      "leading-[1.2]",
+      "uppercase"
+    );
+  });
+  button.forEach((style) => {
+    style.classList.add(
+      "text-white",
+      "font-oswald",
+      "text-fs-6",
+      "tracking-[1px]",
+      "uppercase",
+      "font-medium",
+      "py-[14px]",
+      "px-[35px]",
+      "clip-polygon-1",
+      "ease-in-out",
+      "transition",
+      "duration-[0.15s]",
+      "cursor-pointer"
+    );
+  });
+  cardButton.forEach((style) => {
+    style.classList.replace("py-[14px]", "py-[15px]");
+    style.classList.replace("px-[35px]", "px-[20px]");
+  });
+  primaryButton.forEach((style) => {
+    style.classList.add("bg-orange", "hover:bg-raisin-black-1");
+  });
+  container.forEach((style) => {
+    style.classList.add("px-[15px]");
+  });
+  galleryList.forEach((style) => {
+    style.classList.add("min-w-[95%]", "snap-center");
+  });
+  teamMemberList.forEach((style) => {
+    style.classList.add(
+      "group",
+      "max-w-[40%]",
+      "transition",
+      "ease-in-out",
+      "duration-[0.15s]"
+    );
+  });
+  teamMember.forEach((style) => {
+    style.classList.add(
+      "relative",
+      "block",
+      "rounded-[50%]",
+      "border-[3px]",
+      "border-raisin-black-3",
+      "hover:border-orange"
+    );
+  });
+  teamMemberImage.forEach((style) => {
+    style.classList.add("w-full", "group-hover:opacity-[0.5]");
+  });
+  materialSymbolsOutlined.forEach((style) => {
+    style.classList.add(
+      "absolute",
+      "top-[50%]",
+      "left-[50%]",
+      "translate-x-[-50%]",
+      "translate-y-[-50%]",
+      "text-orange",
+      "text-[50px]",
+      "opacity-0",
+      "group-hover:opacity-100",
+      "transition",
+      "ease-in-out",
+      "duration-[0.15s]"
+    );
+  });
+  footerMenuLink.forEach((style) => {
+    style.classList.add(
+      "px-[10px]",
+      "py-[5px]",
+      "hover:text-orange",
+      "transition",
+      "ease-in-out",
+      "duration-[0.15s]"
+    );
+  });
+  footerQuickLinksList.forEach((style) => {
+    style.classList.add(
+      "py-[5px]",
+      "px-[10px]",
+      "transition",
+      "ease-in-out",
+      "duration-[0.15s]",
+      "tracking-[1px]",
+      "hover:text-orange"
+    );
+  });
+  footerSocialItem.forEach((style) => {
+    style.classList.add(
+      "bg-xiketic",
+      "text-light-gray",
+      "w-[35px]",
+      "h-[35px]",
+      "grid",
+      "place-items-center",
+      "text-[14px]",
+      "rounded-[50%]",
+      "hover:text-orange",
+      "transition",
+      "ease-in-out",
+      "duration-[0.15s]"
+    );
+  });
+
+  searchButton.classList.remove("py-[14px]", "px-[35px]");
+  goTopButton.classList.remove("py-[14px]", "px-[35px]");
+};
+
+openMenuIcon.addEventListener("click", () => {
+  navbar.classList.remove("hidden");
+  navbar.classList.add("right-0", "transition", "ease-out", "duration-[0.25s]");
+  overlay.classList.add(
+    "pointer-events-auto",
+    "bg-overlay",
+    "transition",
+    "ease-out",
+    "duration-[0.25s]"
+  );
+});
+
+const navElementArray = [closeMenuIcon, overlay];
+navElementArray.forEach((element) => {
+  element.addEventListener("click", () => {
+    navbar.classList.add("hidden");
+    overlay.classList.remove(
+      "pointer-events-auto",
+      "bg-overlay",
+      "transition",
+      "ease-out",
+      "duration-[0.25s]"
+    );
+  });
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 800) {
+    // Show the button when scrolled 200px down
+    goTopButton.classList.remove("hidden");
+  } else {
+    // Hide the button when near the top
+    goTopButton.classList.add("hidden");
+  }
+});
+goTopButton.addEventListener("click", (element) => {
+  element.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+applyUiStyle();
---
+++ tailwind.config.js
@@ -0,0 +1,153 @@
+/** @type {import('tailwindcss').Config} */
+const textShadow = require("tailwindcss-textshadow");
+
+const plugin = require("tailwindcss/plugin");
+
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        header: "0 3px 27px hsla(0, 0%, 0%, 0.5)",
+        navbar: "0px 2px 8px hsla(0, 0%, 0%, 0.5)",
+        card: "0px 2px 4px hsla(0,0%,0%,0.2)",
+      },
+      textShadow: {
+        text: "0px 7px hsla(0, 0%, 0%, 0.4)",
+      },
+      fontSize: {
+        "fs-1": "54px",
+        "fs-2": "34px",
+        "fs-3": "30px",
+        "fs-4": "26px",
+        "fs-5": "22px",
+        "fs-6": "20px",
+        "fs-7": "18px",
+        "fs-8": "15px",
+        "fs-9": "14px",
+        "fs-10": "13px",
+        "fs-11": "12px",
+      },
+      backgroundImage: {
+        "hero-banner": "url('./assets/images/hero-banner.jpg')",
+      },
+      fontFamily: {
+        refault: ["Refault"],
+        oswald: ["Oswald", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      aspectRatio: {
+        "2/1.7": "2 / 1.7",
+      },
+      colors: {
+        "raisin-black-1": "hsl(234, 14%, 14%)",
+        "raisin-black-2": "hsl(231, 12%, 12%)",
+        "raisin-black-3": "hsl(228, 12%, 17%)",
+        "eerie-black": "hsl(240, 11%, 9%)",
+        "light-gray": "hsl(0, 0%, 80%)",
+        platinum: "hsl(0, 4%, 91%)",
+        xiketic: "hsl(275, 24%, 10%)",
+        orange: "hsl(31, 100%, 51%)",
+        onyx: "hsl(240, 5%, 26%)",
+        custom: "hsla(0, 0%, 100%, .1)",
+        overlay: "hsla(0, 0%, 0%, 0.7)",
+      },
+      transition: {
+        "transition-1": "0.15s ease-in-out",
+        "transition-2": "0.15s ease-in",
+        "transition-3": "0.25s ease-out",
+      },
+    },
+  },
+  plugins: [
+    textShadow,
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        "@font-face": {
+          fontFamily: "Refault",
+          src: `url('../assets/fonts/REFAULT.woff') format('woff');`,
+          fontStyle: "italic",
+          fontWeight: "normal",
+          textRendering: "optimizeLegibility",
+        },
+
+        ".scrollbar-thin": {
+          "&::-webkit-scrollbar": {
+            width: "8px",
+          },
+          "&::-webkit-scrollbar-track": {
+            background: "hsl(231, 12%, 12%)",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            background: "hsl(31, 100%, 51%)",
+            borderRadius: "10px",
+          },
+        },
+
+        ".has-scrollbar": {
+          "&::-webkit-scrollbar": {
+            height: "6px",
+          },
+          "&::-webkit-scrollbar-button": {
+            width: "40px",
+          },
+        },
+        ".section-title": {
+          position: "relative",
+          "&::before, &::after": {
+            content: '""',
+            position: "absolute",
+            bottom: "-20px",
+            left: "50%",
+            transform: "translateX(-50%)",
+            height: "5px",
+            width: "120px",
+            background: "hsl(31, 100%, 51%)",
+            borderRadius: "0 0 5px 5px",
+          },
+          "&::before": {
+            bottom: "-23px",
+            height: "4px",
+            width: "30px",
+          },
+        },
+        ".footer-brand-wrapper": {
+          "&::after": {
+            content: '""',
+            position: "absolute",
+            bottom: "-4px",
+            left: "0",
+            height: "1px",
+            width: "100%",
+            background: "hsla(0, 0%,50%,0.2)",
+          },
+        },
+        // Polygon clip-path utilities
+        ".clip-polygon-1": {
+          clipPath: "polygon(90% 0, 100% 34%, 100% 100%, 10% 100%, 0 66%, 0 0)",
+        },
+        ".clip-polygon-2": {
+          clipPath: "polygon(0 0, 100% 0%, 82% 100%, 0% 100%)",
+        },
+        ".clip-polygon-3": {
+          clipPath: "polygon(0 0, 100% 0%, 100% 100%, 18% 100%)",
+        },
+        ".clip-polygon-4": {
+          clipPath: "polygon(96% 0, 100% 36%, 100% 100%, 4% 100%, 0 66%, 0 0)",
+        },
+        ".clip-polygon-5": {
+          clipPath: "polygon(75% 0, 100% 25%, 100% 100%, 25% 100%, 0 75%, 0 0)",
+        },
+        ".clip-polygon-6": {
+          clipPath: "polygon(93% 0, 100% 30%, 100% 100%, 7% 100%, 0 63%, 0 0)",
+        },
+        ".clip-polygon-7": {
+          clipPath: "polygon(70% 0, 100% 30%, 100% 100%, 30% 100%, 0 63%, 0 0)",
+        },
+        ".clip-polygon-8": {
+          clipPath: "polygon(70% 0, 100% 30%, 100% 100%, 30% 100%, 0 70%, 0 0)",
+        },
+      });
+    }),
+  ],
+};
